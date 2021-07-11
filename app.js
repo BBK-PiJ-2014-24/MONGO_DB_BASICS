796fb0f655dc832f936744b032658e5808e976de
Stewart Newnham <git@newnham.com>
2021-07-11 18:07:39 +0800

    Added commentary

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoBongo = require('./mongo');
+// const mongoBongo = require('./mongo');
+const mongoBongo = require('./mongoose');
 
 
 const app = express();
--- mongo.js
+++ mongo.js
@@ -1,24 +1,37 @@
+// Node Connection to MongoDB
+// ==========================
+
 const MongoClient = require('mongodb').MongoClient;
 
 const url = 'mongodb+srv://user1:Edcrfv123@cluster0.3ygns.mongodb.net/productsDB?retryWrites=true&w=majority';
 
+
+// Queries
+//--------
 const createProduct = async (req, res, next) => {
+
+    // Prepare data for insert
     const newProduct = {
         name: req.body.name,
         price: req.body.price,
     };
 
-    const client = new MongoClient(url);
+    const client = new MongoClient(url);// create MongoDB client
+
+    // Client Connection must be 1) Wrapped in Try and 2) Await
     try {
-        await client.connect();
-        const db = client.db();
-        const result = db.collection('productsCollection').insertOne(newProduct);
+        await client.connect(); // use client to connect to MDB
+        const db = client.db(); // access the database
+        const result = db.collection('productsCollection').insertOne(newProduct); // insert data
     } catch (err) {
         return res.json({ message: 'Error with connection' });
     };
 
+    // Close MDB connection
+    // --------------------
     client.close();
 
+    // JSON output
     res.json(newProduct);
 };
 
